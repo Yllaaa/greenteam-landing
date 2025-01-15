91885373794a555a7e3c0084cdaf8d6fc973f4ed
Moustafa <moustafaadel097@gmail.com>
2025-01-16 00:50:55 +0200

    deployError

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,18 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
-// import Footer from "@/components/zFooter/Footer";
-// import Navbar from "@/components/aaaNavbar/Navbar";
 
 export default async function RootLayout({
   children,
   params,
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: { locale: any };
 }) {
-  // No need to await params
-  const { locale } = params;
+  const { locale } = await params;
 
   // Fetch messages server-side
   const messages = await getMessages(locale as any);
