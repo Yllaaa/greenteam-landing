8d5cfdc5a524758dcad7baec73099c47a8135e1e
Moustafa <moustafaadel097@gmail.com>
2025-01-17 01:19:58 +0200

    add about section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from "./page.module.css";
 import Hero from "@/components/home/hero/Hero";
+import AboutH from "@/components/home/about/AboutH";
 function page() {
   return (
     <>
       <section className={styles.container}>
         <div className={styles.hero}>
           <Hero />
+        </div>
+        <div className={styles.aboutH}>
+          <AboutH />
         </div>
       </section>
     </>
---
+++ src/components/home/about/AboutH.tsx
@@ -0,0 +1,70 @@
+"use client";
+import React from "react";
+import styles from "./aboutH.module.css";
+import Link from "next/link";
+import { useLocale } from "next-intl";
+
+function AboutH() {
+  const locale = useLocale();
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.contentBody}>
+          <div className={styles.title}></div>
+          <div className={styles.content}></div>
+        </div>
+        <div className={styles.allCardsContainer}>
+          <div className={styles.cardsUpper}>
+            <div
+              className={`${styles.card} ${styles.card1} ${styles.cardGreen}`}
+            >
+              Sustainability
+            </div>
+            <div
+              className={`${styles.card} ${styles.card2} ${styles.cardDark}`}
+            >
+              Comunity
+            </div>
+            <div
+              className={`${styles.card} ${styles.card3} ${styles.cardGreen}`}
+            >
+              Eco-friendly
+            </div>
+            <div
+              className={`${styles.card} ${styles.card4} ${styles.cardDark}`}
+            >
+              EcoCommunity
+            </div>
+          </div>
+          <div className={styles.cardsLower}>
+            <div
+              className={`${styles.card} ${styles.card5} ${styles.cardGreen}`}
+            >
+              Collaboration
+            </div>
+            <div
+              className={`${styles.card} ${styles.card6} ${styles.cardBlue}`}
+            >
+              Social Impact
+            </div>
+            <div
+              className={`${styles.card} ${styles.card7} ${styles.cardDark}`}
+            >
+              Design
+            </div>
+            <div
+              className={`${styles.card} ${styles.card8} ${styles.cardDark}`}
+            >
+              Green Intiative
+            </div>
+          </div>
+        </div>
+        <div className={styles.btn}>
+          <Link href={`${locale}/explore`}>ABOUT US</Link>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AboutH;
