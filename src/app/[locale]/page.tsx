67d8e7e65d2c832940cf4d52ff4bf618f3cdfab9
Moustafa <moustafaadel097@gmail.com>
2025-01-24 13:56:28 +0200

    landingV3

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./page.module.css";
-import Hero from "@/components/home/hero/Hero";
+import Hero from "@/components/Landing_Page/home/hero/Hero";
 function page() {
   return (
     <>
