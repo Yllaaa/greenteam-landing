852ebd02b6c4442ab9b9aafd624590cb25a7cd35
Moustafa <moustafaadel097@gmail.com>
2025-02-01 18:17:17 +0200

    update footer v1

--- src/components/platform/zfooter/Footer.tsx
+++ src/components/platform/zfooter/Footer.tsx
@@ -1,12 +1,16 @@
 "use client";
 import React from "react";
 import styles from "./footer.module.css";
+import footLogo from "@/../public/logo/foot.png";
+import Image from "next/image";
 
 function Footer() {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.upper}></div>
+        <div className={styles.upper}>
+          <Image src={footLogo} alt="footLogo" />
+        </div>
         <div className={styles.lower}></div>
       </div>
     </>
