6a67fe0587c8b9b66a7c60e47aebc2b4ecc842d7
Moustafa <moustafaadel097@gmail.com>
2025-02-01 18:19:56 +0200

    footer v1

--- src/components/platform/zfooter/Footer.tsx
+++ src/components/platform/zfooter/Footer.tsx
@@ -3,13 +3,24 @@ import React from "react";
 import styles from "./footer.module.css";
 import footLogo from "@/../public/logo/foot.png";
 import Image from "next/image";
+import cirle from "@/../public/ZPLATFORM/footer/circle.svg";
 
 function Footer() {
   return (
     <>
       <div className={styles.container}>
         <div className={styles.upper}>
-          <Image src={footLogo} alt="footLogo" />
+          <div className={styles.content}>
+            <Image src={footLogo} alt="footLogo" className={styles.logo} />
+            <h2 className={styles.text}>Help us to make sustainable culture</h2>
+            <div className={styles.actions}>
+              <button className={styles.button}>Join us</button>
+              <button className={styles.button}>Donate</button>
+            </div>
+          </div>
+          <div className={styles.circle}>
+            <Image src={cirle} alt="circles" className={styles.circleImage} />
+          </div>
         </div>
         <div className={styles.lower}></div>
       </div>
