93f1e9bfc3bb54c38ffc28ef56b26d1ecaedf3ac
Moustafa <moustafaadel097@gmail.com>
2025-02-04 17:59:29 +0200

    update login

--- src/components/AUTH/Login/LoginForm.tsx
+++ src/components/AUTH/Login/LoginForm.tsx
@@ -80,6 +80,8 @@ function LoginForm() {
           {
             headers: {
               "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
             },
           }
         )
