82ad6a70d866113fb82071ff6c79136892aaef92
Moustafa <moustafaadel097@gmail.com>
2025-03-06 03:25:06 +0200

    chat implemented with weired behavior

--- src/app/[locale]/(web)/(sub)/personal/newPost/page.tsx
+++ src/app/[locale]/(web)/(sub)/personal/newPost/page.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import styles from "./newPost.module.css";
 import AddNew from "@/components/platform/addNew/post/AddNew";
-<<<<<<< HEAD
-=======
-import Forums from "@/components/platform/addNew/forums/Forum";
->>>>>>> db2d0d82ce1732d0ee38271beba110cca6d1bf48
 
 function page() {
   return (
