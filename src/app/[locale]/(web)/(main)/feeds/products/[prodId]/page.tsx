a973ece851166f1447c028e8fa9a0fcfbb198d2b
Moustafa <moustafaadel097@gmail.com>
2025-03-28 13:13:58 +0200

    prod

--- src/app/[locale]/(web)/(main)/feeds/products/[prodId]/page.tsx
+++ src/app/[locale]/(web)/(main)/feeds/products/[prodId]/page.tsx
@@ -2,15 +2,16 @@ import SingleProduct from "@/components/platform/posts/products/singleProduct/Si
 import React from "react";
 import styles from "./prodId.module.css";
 
-async function page(params: Promise<{ prodId: string }>) {
+async function page({ params }: { params: Promise<{ prodId: string }> }) {
   const { prodId } = await params;
   return (
     <>
-      <div className={styles.container}>
-        <SingleProduct prodId={prodId} />
-      </div>
+      {prodId && (
+        <div className={styles.container}>
+          <SingleProduct prodId={prodId} />
+        </div>
+      )}
     </>
   );
 }
-
 export default page;
--- src/components/platform/posts/products/modal/MessageModal.tsx
+++ src/components/platform/posts/products/modal/MessageModal.tsx
@@ -1,19 +1,77 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import ToastNot from "@/Utils/ToastNotification/ToastNot";
 import { getToken } from "@/Utils/userToken/LocalToken";
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import styles from "./MessageModal.module.css";
 import { useForm } from "react-hook-form";
 import useOutsideClick from "@/hooks/clickoutside/useOutsideClick";
 import preventBackgroundScroll from "@/hooks/preventScroll/preventBackroundScroll";
 import axios from "axios";
+import io, { Socket } from "socket.io-client";
 
 function MessageModal(props: any) {
+  const SOCKET_URL = "https://greenteam.yllaaa.com/api/v1/chat";
   const token = getToken();
   const accessToken = token ? token.accessToken : "";
+  const socketRef = useRef<Socket | null>(null);
 
   const { setMessage, sellerId, sellerType } = props;
 
+  // useEffect(() => {
+  //   if (!sellerId) return;
+
+  //   // Function to initialize WebSocket connection
+  //   const connectSocket = () => {
+  //     socketRef.current = io(SOCKET_URL, {
+  //       transports: ["websocket"],
+  //       auth: { token: accessToken },
+  //     });
+
+  //     socketRef.current.on("connect", () =>
+  //       console.log("Connected to WebSocket")
+  //     );
+
+  //     socketRef.current.on("newMessage", (message: any) => {
+  //       console.log("testNew", message);
+
+  //       if (message.conversationId === chatId) {
+  //         console.log("Received message:", message);
+  //         setMessages((prev) => [...prev, message]);
+  //       }
+  //       console.log("user:", chatId);
+
+  //       console.log("New message:", message);
+  //     });
+
+  //     socketRef.current.on("exception", (error: any) =>
+  //       console.error("Socket exception:", error)
+  //     );
+  //   };
+
+  //   connectSocket();
+
+  //   return () => {
+  //     // clearInterval(interval);
+  //     socketRef.current?.disconnect();
+  //   };
+  // }, [selectedUser]);
+
+  useEffect(() => {
+    const connectSocket = () => {
+      socketRef.current = io(SOCKET_URL, {
+        transports: ["websocket"],
+        auth: { token: accessToken },
+      });
+
+      socketRef.current.on("connect", () =>
+        console.log("Connected to WebSocket")
+      );
+    };
+    connectSocket();
+  }, [sellerId]);
+
+  const [newMessage, setNewMessage] = useState<string>("");
   const closeModal = useCallback(() => {
     setMessage(false);
   }, [setMessage]);
@@ -27,8 +85,33 @@ function MessageModal(props: any) {
       preventBackgroundScroll(false);
     };
   }, []);
+  const sendMessage = async () => {
+    if (!newMessage.trim() || !sellerId || !socketRef.current?.connected)
+      return;
+
+    const messageData:
+      | {
+          content: string;
+          recipient: { id: string; type: string };
+        }
+      | any = {
+      content: newMessage,
+      recipient: { id: sellerId, type: sellerType },
+    };
+
+    socketRef.current.emit("sendMessage", messageData, (response: any) => {
+      if (response?.success) {
+        setNewMessage("");
+        closeModal();
+      } else {
+        console.log(response);
+        console.error("Failed to send message", response?.error);
+      }
+    });
+  };
+
   // Form handling
-  const { register, reset, handleSubmit } = useForm<any>({
+  const { reset, handleSubmit } = useForm<any>({
     defaultValues: {
       content: "",
       recipient: {
@@ -80,10 +163,11 @@ function MessageModal(props: any) {
             <textarea
               placeholder="Add your experiences and tips to make a better future."
               className={styles.textArea}
-              {...register("content", { required: true })}
+              // {...register("content", { required: true })}
+              onChange={(e) => setNewMessage(e.target.value)}
             />
             {/* Submit Button */}
-            <button onClick={handleSubmit(onSubmit)}>SEND</button>
+            <button onClick={handleSubmit(sendMessage)}>SEND</button>
           </form>
         </div>
       </div>
