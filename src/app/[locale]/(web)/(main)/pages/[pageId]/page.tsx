38ecb891045326f116ed157f46dcdea4164c846f
Moustafa <moustafaadel097@gmail.com>
2025-04-07 21:06:36 +0200

    pages v0

--- src/app/[locale]/(web)/(main)/pages/[pageId]/page.tsx
+++ src/app/[locale]/(web)/(main)/pages/[pageId]/page.tsx
@@ -1,8 +1,8 @@
 import PagesBody from "@/components/platform/sections/sec_pages/body/PagesBody";
 import React from "react";
 
-function page(params: { params: { pageId: string } }) {
-  const { pageId } = params.params;
+async function page(params: { params: Promise<{ pageId: string }> }) {
+  const { pageId } = await params.params;
   return (
     <>
       <PagesBody pageId={pageId} />
