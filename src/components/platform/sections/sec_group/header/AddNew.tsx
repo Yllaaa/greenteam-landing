326d663537acf12d82d5c02fa7a30090236fa669
Moustafa <moustafaadel097@gmail.com>
2025-04-10 18:47:07 +0200

    test v0

--- src/components/platform/sections/sec_group/header/AddNew.tsx
+++ src/components/platform/sections/sec_group/header/AddNew.tsx
@@ -1,27 +1,28 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import styles from "./header.module.scss";
 import cover from "@/../public/ZPLATFORM/groups/cover.png";
-import ImageUpload from "@/Utils/imageUploadComponent/clickToUpload/ImageUpload";
+// import ImageUpload from "@/Utils/imageUploadComponent/clickToUpload/ImageUpload";
 import { useForm } from "react-hook-form";
 import ToastNot from "@/Utils/ToastNotification/ToastNot";
 import { postRequest } from "@/Utils/backendEndpoints/backend-requests";
 import Image from "next/image";
+// import FileUpload from "@/Utils/imageUploadComponent/clickToUpload/ImageUpload";
 
 function AddNew() {
-  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
-  const handleImagesSelected = (files: File[]) => {
-    setSelectedFiles((prev) => [...prev, ...files]);
+  // const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+  // const handleImagesSelected = (files: File[]) => {
+  //   setSelectedFiles((prev) => [...prev, ...files]);
 
-    // In a real application, you would typically upload these files to your server
-    console.log("Files selected:", files);
-  };
+  //   // In a real application, you would typically upload these files to your server
+  //   console.log("Files selected:", files);
+  // };
   // Form handling
   const { register, reset, handleSubmit } = useForm<any>({
     defaultValues: {
       content: "",
-      images: selectedFiles,
+      // images: selectedFiles,
       creatorType: "group_user",
     },
   });
@@ -61,11 +62,11 @@ function AddNew() {
           />
         </div>
         <div className={styles.postActions}>
-          <ImageUpload
+          {/* <FileUpload
             onImagesSelected={handleImagesSelected}
             maxImages={4}
             maxSizeInMB={2}
-          />
+          /> */}
           <button type="submit" className={styles.postButton}>
             Post
           </button>
