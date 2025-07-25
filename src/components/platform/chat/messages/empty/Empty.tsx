222d5f4765c602814a6a831425915febe69cfa3f
Moustafa <moustafaadel097@gmail.com>
2025-07-25 12:26:31 +0300

    chat user

--- src/components/platform/chat/messages/empty/Empty.tsx
+++ src/components/platform/chat/messages/empty/Empty.tsx
@@ -1,15 +1,19 @@
+"use client"
 import Image from 'next/image'
 import styles from './empty.module.scss'
 import inboxIcon from '@/../public/chat/inbox.svg'
-
+import { useSearchParams } from 'next/navigation'
 export default function Empty() {
+    const params = useSearchParams()
+    const fullName = params.get("chatFullName")
+    console.log(params.get("chatFullName"))
     return (
         <div className={styles.empty}>
             <div className={styles.icon}>
                 <Image src={inboxIcon} alt="empty" />
             </div>
-            <div className={styles.title}>Your messages</div>
-            <div className={styles.details}>Select a person to display their chat or start a new conversation.</div>
+            <div className={styles.title}>{fullName? fullName : "Your messages"}</div>
+            <div className={styles.details}>{fullName? "Chat with " + fullName : "Select a person to display their chat or start a new conversation."}</div>
         </div>
     )
 }
